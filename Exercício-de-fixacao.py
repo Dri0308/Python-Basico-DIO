fdcfc9b4be07d2935814f614b1fa46a6efdd625a
Dri0308 <adriana.rn030812@gmail.com>
2025-08-21 21:36:53 -0300

    strings e listas

--- Exercício-de-fixacao.py
+++ Exercício-de-fixacao.py
@@ -1,1 +1,16 @@
-print("Hello, World!")
+#print("Hello, World!")
+nome = "adriana"
+print(nome.title())#Com este método, apenas a primeira letra de cada palavra é maiúscula.
+
+nome = "Adriana Rocha"
+print(nome.lstrip(" "))
+print(nome.rstrip())
+
+materias_da_semana = ["Python", "Banco de Dados", "Github", "Ingles"]
+print(materias_da_semana[3])
+print(materias_da_semana[-2])
+print(materias_da_semana[ 2:4])
+
+SKZ_Members = ["Changbin", "Felix", "Hunjin", "I.N", "Bang Chan", "Lee Know", "Seungmin", "Han"]
+for indice, SKZ_Members in enumerate(SKZ_Members):
+    print(f'{indice} : {SKZ_Members}')
